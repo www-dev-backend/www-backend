5155621e2fa4186e6a51a000da3cf1f39df70585
anjwoc <anjwoc@naver.com>
2022-11-09 01:06:37 +0900

    feat: BaseRepository 세팅

---
+++ src/main/java/com/www/backend/common/entity/BaseTimeEntity.java
@@ -0,0 +1,13 @@
+package com.www.backend.common.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/www/backend/common/repository/BaseRepositoryImpl.java
@@ -0,0 +1,17 @@
+package com.www.backend.common.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import javax.persistence.EntityManager;
+
+public class BaseRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID> implements BaseRepository<T, ID> {
+    EntityManager em;
+    JPAQueryFactory query;
+
+    public BaseRepositoryImpl(Class<T> domainClass, EntityManager em) {
+        super(domainClass, em);
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
+}
