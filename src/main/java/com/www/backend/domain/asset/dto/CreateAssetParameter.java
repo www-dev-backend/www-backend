3bd124749dae8fea237815f47ac27cb76ce4e823
anjwoc <anjwoc@naver.com>
2022-12-11 00:03:53 +0900

    feat: Create Asset API Validation 수정

--- src/main/java/com/www/backend/domain/asset/dto/CreateAssetParameter.java
+++ src/main/java/com/www/backend/domain/asset/dto/CreateAssetParameter.java
@@ -4,17 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter @Setter
 public class CreateAssetParameter {
-    @NotBlank
+    @NotBlank(message = "작품 Genre는 필수 값입니다.")
     private String genre;
 
-    @NotBlank
+    @NotBlank(message = "작품 Type은 필수 값입니다.")
     private String type;
 
-    @NotBlank
+    @NotBlank(message = "URL은 필수 값입니다.")
     private String url;
 
+    @NotBlank(message = "대표 작품 여부는 필수 값입니다.")
     private Boolean isMain;
 }
